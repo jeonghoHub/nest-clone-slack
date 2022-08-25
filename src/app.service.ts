e90913ace65ba4b96fdd2c723ff87c9fdb5e1ed4
jeonghoHub <tjsrsdqq4@gmail.com>
2022-08-25 23:47:13 +0900

    폴더구조 변경

--- src/app.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { UsersService } from './users/users.service';
-
-@Injectable()
-export class AppService {}
--- src/users/users.service.ts
+++
@@ -1,6 +0,0 @@
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class UsersService {
-  postUsers(email: string, nickname: string, password: string) {}
-}
